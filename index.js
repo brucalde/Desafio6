b009e0c28cb52af8b047b6916cd6a93bad6790dc
brucalde <brunocsir@gmail.com>
2022-10-10 22:44:22 -0300

    Completo

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const { Server: HttpServer } = require('http')
+const { Server: Socket } = require('socket.io')
+
+
+
+const Mensajes = require('./model/modelomensaje')
+const mensajes = new Mensajes('mensajes.json')
+const Productos = require('./model/modelos')
+const producto = new Productos()
+console.log(producto)
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+
+
+
+io.on('connection', async socket => {
+    console.log('Nuevo cliente conectado!');
+
+    // carga inicial de productos
+    socket.emit('productos', producto.traeTodo
+    ());
+
+    // actualizacion de productos
+    socket.on('update', prod => {
+        producto.agregaProd(prod)
+        io.sockets.emit('productos', producto.traeTodo());
+    })
+
+    // carga inicial de mensajes
+    socket.emit('mensajes', await mensajes.listarTodo());
+
+    // actualizacion de mensajes
+    socket.on('nuevoMensaje', async msj => {
+        msj.fyh = new Date().toLocaleString()
+        await mensajes.guardarMensajes(msj)
+        io.sockets.emit('mensajes', await mensajes.listarTodo());
+    })
+});
+
+// middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+
+const PORT = 8080
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
---
+++ model/modelomensaje.js
@@ -0,0 +1,39 @@
+const {promises: fs} = require('fs')
+
+
+class Mensajes{
+    constructor(ruta) {
+        this.ruta = ruta
+    }
+
+    async listarTodo() {
+        try {
+            const objs = await fs.readFile(this.ruta, 'utf-8')
+            return JSON.parse(objs)
+        } catch (error) {
+            return []
+        }
+    }
+    async guardarMensajes(msj) {
+        const objs = await this.listarTodo()
+
+        let newId
+        if (objs.length == 0) {
+            newId = 1
+        } else {
+            newId = objs[objs.length - 1].id + 1
+        }
+
+        const newObj = { ...msj, id: newId }
+        objs.push(newObj)
+
+        try {
+            await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2))
+            return newId
+        } catch (error) {
+            throw new Error(`Error al guardar: ${error}`)
+        }
+    }
+
+}
+module.exports = Mensajes
